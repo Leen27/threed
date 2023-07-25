6e398706290c388ec2a55056098bc6a17dcde635
林京秦 <linjingqin@fline88.com>
2023-07-25 10:14:30 +0800

    fix

--- packages/my-threejs/src/materials/ColorMaterial.ts
+++
@@ -1,9 +0,0 @@
-import Color from "@my-threejs/core/Color";
-
-export default class ColorMaterial {
-	color: Color
-
-	constructor(hex: number, opacity?: number) {
-		this.color = new Color( (opacity ? (opacity * 0xff) << 24 : 0xff000000) | hex );
-	}
-}
